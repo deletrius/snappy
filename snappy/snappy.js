a789d5a8e3e1b5302f2ab77b0f17707cf92815f5
G C <deletrius_00@hotmail.com>
2015-07-26 11:20:30 -0600

    - working pick and choose

--- snappy/snappy.js
+++ snappy/snappy.js
@@ -3,6 +3,8 @@ ImageCollection = new Mongo.Collection("imageCollection");
 
 ChatCollection = new Mongo.Collection("Messages");
 
+var arrOptions = new Array();
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -25,35 +27,42 @@ if (Meteor.isClient) {
 		
 		document.getElementById("header").innerHTML = "";
 		
-		for (i = 0; i < 1; i++) { 
+		for (i = 0; i < 10; i++) { 
+			
+			arrOptions[i] = data.SearchResults.Products[i].Description.ImageURL;
 			var img = document.createElement("img");
 			img.src = data.SearchResults.Products[i].Description.ImageURL;
 			img.width = 200;
 			img.height = 200;
 			
-			// var h = document.createElement("H1")                // Create a <h1> element
+			var h = document.createElement("H1")                // Create a <h1> element
 			var t = "Name: " + data.SearchResults.Products[i].Description.Name;     // Create a text node
-			// h.appendChild(t);
+			var t1 = document.createTextNode("Name: " + data.SearchResults.Products[i].Description.Name);     // Create a text node
+			h.appendChild(t1);
 
-			// var h2 = document.createElement("H1")                // Create a <h1> element
+			var h2 = document.createElement("H1")                // Create a <h1> element
 			var t2 = "Sale Price: " + data.SearchResults.Products[i].Price.DisplayPrice;     // Create a text node
-			// h2.appendChild(t2);
+			var t22 = document.createTextNode("Sale Price: " + data.SearchResults.Products[i].Price.DisplayPrice);     // Create a text node
+			h2.appendChild(t22);
 			
-			// var h4 = document.createElement("H3")                // Create a <h1> element
+			var h4 = document.createElement("H3")                // Create a <h1> element
 			var t4 = "Original Price: " + data.SearchResults.Products[i].Price.CutPrice;     // Create a text node
-			// h4.appendChild(t4);
+			var t44 = document.createTextNode("Original Price: " + data.SearchResults.Products[i].Price.CutPrice);     // Create a text node
+			h4.appendChild(t44);
 			
-			// var h3 = document.createElement("H1")                // Create a <h1> element
+			var h3 = document.createElement("H1")                // Create a <h1> element
 			var t3 = "Rating (/5): " + data.SearchResults.Products[i].Description.ReviewRating.Rating;     // Create a text node
-			// h3.appendChild(t3);
+			var t33 = document.createTextNode("Rating (/5): " + data.SearchResults.Products[i].Description.ReviewRating.Rating);     // Create a text node
+			h3.appendChild(t33);
 			
-			// http://www.sears.com/disney-dcm-1-classic-mickey-waffle-maker-brushed/p-SPM1177668414
+			http://www.sears.com/disney-dcm-1-classic-mickey-waffle-maker-brushed/p-SPM1177668414
 			
 			
 			
-			// var h5 = document.createElement("H2")                // Create a <h1> element
+			var h5 = document.createElement("H2")                // Create a <h1> element
 			var t5 = "http://www.sears.com/" + data.SearchResults.Products[i].Description.Name + "/p-" +data.SearchResults.Products[i].Id.PartNumber;     // Create a text node
-			// h5.appendChild(t5);
+			var t55 = document.createTextNode("http://www.sears.com/" + data.SearchResults.Products[i].Description.Name + "/p-" +data.SearchResults.Products[i].Id.PartNumber);     // Create a text node
+			h5.appendChild(t55);
 			
 			// var a = document.createElement('a');
 			// var linkText = document.createTextNode(data.SearchResults.Products[i].Description.Name);
@@ -62,43 +71,56 @@ if (Meteor.isClient) {
 			// a.href = "http://www.sears.com/" + data.SearchResults.Products[i].Description.Name + "/p-" +data.SearchResults.Products[i].Id.PartNumber;
 			// a.target = "_blank";
 			
-			// var hr = document.createElement("hr")
+			var hr = document.createElement("hr")
 			
-			// var src = document.getElementById("header");
-   //    var src = document.createElement("productDetail");
-			// src.appendChild(img);
-			// src.appendChild(h);
-			// src.appendChild(h2);
-			// src.appendChild(h4);
-			// src.appendChild(h3);
-			// src.appendChild(h5);
-			// src.appendChild(a);
-			// src.appendChild(hr);
+			button = document.createElement("input");
+			button.id = i;
+			button.type = "button";
+			button.value = i;
+			button.onclick = (function(opt, price, name, buy, original) {
+    return function() {
+       showParam(opt, price, name, buy, original);
+    };
+})(data.SearchResults.Products[i].Description.ImageURL, "Sale Price: " + data.SearchResults.Products[i].Price.DisplayPrice, "Name: " + data.SearchResults.Products[i].Description.Name, "http://www.sears.com/" + data.SearchResults.Products[i].Description.Name + "/p-" +data.SearchResults.Products[i].Id.PartNumber, "Original Price: " + data.SearchResults.Products[i].Price.CutPrice);
+			
+			var src = document.getElementById("header");
+      //var src = document.createElement("productDetail");
+			src.appendChild(img);
+			 src.appendChild(h);
+			 src.appendChild(h2);
+			 src.appendChild(h4);
+			 src.appendChild(h3);
+			 src.appendChild(h5);
+			 src.appendChild(button);
+			// //src.appendChild(a);
+			 src.appendChild(hr);
+			 
+			 
 
-      ChatCollection.insert({
-        imageUrl: img.src,
-        timestamp: Date.now()
-      });
-      ChatCollection.insert({
-        message: t,
-        timestamp: Date.now()
-      });
-      ChatCollection.insert({
-        message: t2,
-        timestamp: Date.now()
-      });
-      ChatCollection.insert({
-        message: t4,
-        timestamp: Date.now()
-      });
-      ChatCollection.insert({
-        message: t3,
-        timestamp: Date.now()
-      });
-      ChatCollection.insert({
-        linkUrl: t5,
-        timestamp: Date.now()
-      });
+      // ChatCollection.insert({
+        // imageUrl: img.src,
+        // timestamp: Date.now()
+      // });
+      // ChatCollection.insert({
+        // message: t,
+        // timestamp: Date.now()
+      // });
+      // ChatCollection.insert({
+        // message: t2,
+        // timestamp: Date.now()
+      // });
+      // ChatCollection.insert({
+        // message: t4,
+        // timestamp: Date.now()
+      // });
+      // ChatCollection.insert({
+        // message: t3,
+        // timestamp: Date.now()
+      // });
+      // ChatCollection.insert({
+        // linkUrl: t5,
+        // timestamp: Date.now()
+      // });
 		}
 		
 		});
@@ -211,4 +233,28 @@ if (Meteor.isServer) {
 
     });
   });
+}
+
+function showParam(opt, price, name, buy, original) {
+    ChatCollection.insert({
+					imageUrl: opt,
+					timestamp: Date.now()
+				  });
+
+      ChatCollection.insert({
+        message: price,
+        timestamp: Date.now()
+      });
+	        ChatCollection.insert({
+        message: original,
+        timestamp: Date.now()
+      });
+      ChatCollection.insert({
+        message: name,
+        timestamp: Date.now()
+      });
+	        ChatCollection.insert({
+        linkUrl: buy,
+        timestamp: Date.now()
+      });
 }
