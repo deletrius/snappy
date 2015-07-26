91f43a99ab9666ade04433da661b56ec7e90429b
Eric Kim <bosung90@gmail.com>
2015-07-26 00:09:02 -0700

    Trying to do upload image and view it

--- snappy/snappy.js
+++ snappy/snappy.js
@@ -1,9 +1,15 @@
+var Images = new FS.Collection("images", {
+  stores: [new FS.Store.FileSystem("images", { path: "~/uploads" })]
+});
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
+
+
   Template.hello.helpers({
-    counter: function () {
+    getCounterSessionValue: function () {
       return Session.get('counter');
     }
   });
@@ -12,8 +18,30 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
+      console.log("you clicked!");
+    }
+
+  });
+
+  Template.uploadImage.events({
+    'change .myFileInput': function (event, template) {
+      FS.Utility.eachFile(event, function (file) {
+        Images.insert(file, function (err, fileObj) {
+          //console.log(fileObj.path);
+          // console.log(fileObj.;
+          console.log("uploading");
+        });
+      });
     }
   });
+
+  Template.imageView.helpers({
+    images: function () {
+      return Images.find(); // Where Images is an FS.Collection instance
+    }
+  });
+
+
 }
 
 if (Meteor.isServer) {
